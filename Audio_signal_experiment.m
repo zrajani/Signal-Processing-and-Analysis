875bb50080085384aea2bb8b0b0bf393eb5039c4
zrajani <zainrajani2207@gmail.com>
2019-09-15 16:44:00 -0400

    Audio Signal Basic Operation

---
+++ Audio_signal_experiment.m
@@ -0,0 +1,21 @@
+% Code Abstract: Performing and Experimenting different operations on Audio
+% Signals
+% Author: Zain Rajani
+% Creation Date: September 15, 2019
+
+%% Initial Screen Commands
+
+clear all;           % Clear the memory of MATLAB e.g. Variables, Workspace,etc.
+close all;           % Close any figures or any external window currently being open
+clc;                 % Clear the command window
+
+%% Reading/ Inputting Audio files in MATLAB
+% For any audio files to be as input in MATLAB it should be in the
+% following formats such as .mp3, .mp4, .mpeg, .wav, .flac, .au, etc.
+% The function used to input the audio files could be audioread or load
+% function use help function_name for more details 
+% For the scope of this code we will take two audio signals namely audio 1
+% and audio 2 which will be 
+
+audio_1=
+audio_2=
