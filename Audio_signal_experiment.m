5deb7fd2edf24c666a64be3613995df026e66a58
zrajani <zainrajani2207@gmail.com>
2019-09-17 22:17:47 -0400

    Updation with volume function

--- Audio_signal_experiment.m
+++ Audio_signal_experiment.m
@@ -1,7 +1,8 @@
-% Code Abstract: Performing and Experimenting different operations on Audio
-% Signals
+% Code Abstract: Performing and Experimenting different operations on Audio 
+%Signal
 % Author: Zain Rajani
 % Creation Date: September 15, 2019
+% Last Modified: September 17, 2019
 
 %% Initial Screen Commands
 
@@ -29,9 +30,11 @@ audio_file_2='SpeechDFT-16-8-mono-5secs.wav';
 %% Getting Information about the audio signals we have inputted
 % Information about audio signal can be obtained using 'audioinfo' command
 % Reading and storing information of the audio files
-
-audio_1_info= audioinfo(audio_file_1);
-audio_2_info= audioinfo(audio_file_2);
+disp('Information of the audio signal')
+disp('Female Voice Information: ')  
+audio_1_info= audioinfo(audio_file_1); % To view the information when the script is running remove the semi-colon
+disp('Male Voice Information: ')
+audio_2_info= audioinfo(audio_file_2); % To view the information when the script is running remove the semi-colon
 
 %% Playing the audio files using the commands 
 % To play the audio that we have taken as input we create a object for
@@ -52,3 +55,55 @@ sound (audio_1_data);
 pause (audio_1_info.Duration)
 sound (audio_2_data);
 pause (audio_2_info.Duration)
+
+%% Plotting of audio signal
+
+t1=0:seconds(1/audio_1_samplef):seconds(audio_1_info.Duration);
+t1 = t1(1:end-1);
+
+t2=0:seconds(1/audio_2_samplef):seconds(audio_2_info.Duration);
+t2 = t2(1:end-1);
+
+figure
+subplot(2,1,1)
+plot(t1,audio_1_data)
+title('Female Voice Data')
+xlabel('Time')
+ylabel('Audio Signal')
+
+
+subplot(2,1,2)
+plot(t2,audio_2_data,'color','r')
+title('Male Voice Data')
+xlabel('Time')
+ylabel('Audio Signal')
+
+%% Increasing the volume of audio signal in MATLAB
+% In this part of the experiment we will try to increase and decrease the
+% the volume of the audio signal.
+% In an event to increase the sound of the audio if the data of the signal
+% is multiplied by a factor it will in turn increase the amplitude (data)
+% value of the signal and the opposite goes to decrease the volume of the
+% audio signal
+% This operation will be performed just on one signal instead of both for
+% illustration purposes
+
+%original audio
+sound (audio_1_data);
+pause (audio_1_info.Duration)
+
+vol_dec_factor= audio_1_data/10; % This will decrease the volume by 10
+vol_inc_factor= audio_1_data*5; % This will increase the volume 5 times
+
+dec_play=audioplayer(vol_dec_factor,audio_1_samplef);
+inc_play=audioplayer(vol_inc_factor,audio_1_samplef);
+
+%Play the decreased volume audio signal
+play(dec_play); 
+pause (audio_1_info.Duration);
+
+%Play the increased volume audio signal 
+play(inc_play);
+pause (audio_1_info.Duration);
+
+%% Flip the audio signals
