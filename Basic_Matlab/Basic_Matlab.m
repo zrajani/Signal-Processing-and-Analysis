13fb0900d3434248c8507d27910fde90eb618c36
Zain Anwar Rajani <zainrajani2207@gmail.com>
2019-10-08 14:07:14 -0400

    Update Basic_Matlab.m

--- Basic_Matlab/Basic_Matlab.m
+++ Basic_Matlab/Basic_Matlab.m
@@ -104,89 +104,46 @@ concatenate_matrix_2=[matrix_A;matrix_B];  % Concatenate row-wise
 
 matrix_C=[matrix_A(1,:); matrix_A(2,2)*matrix_A(2,:); matrix_A(3,:)];
 
-%% Question 11
+%% Vector Creation from 0 to 500 with 100 samples
+
+vector_100_samples=0:5.01:500;
+
+%% Creating a 2D Plot 
+% We first define the sine function
+xmin=0;
+xmax=6.3;
+ymin=-1.5;
+ymax=1.5;
+n=0:0.001:2*pi;
+x=sin(n);
+y=cos(n);
+plot(n,x,'--c','Linewidth',2);
+hold on
+plot(n,y,':b','Linewidth',2);
+title('Sine Function from 0 to 2{\pi}');
+ylabel('Amplitude'); xlabel('Time (in radians)');
+xticks([0 pi/2 pi 3*pi/2 2*pi])
+xticklabels({'0','\pi/2','\pi','3\pi/2','2\pi'});
+grid on; axis ([xmin xmax ymin ymax]);
+legend ('Sine Wave','Cosine Wave');
+hold off
+%% Alternative to plot 2D graph
 t=0:0.01:3*pi; %Time range
 
 Npts=512; %No of Points to plot
 y=linspace(0,2*pi,Npts); %Creates Number of point
 omega_1=2;
 x=sin(omega_1*y); %Computing of Sine Function
-z=cos(omega_1*y);
-a=tan(y);
+z=cos(omega_1*y); %Computing of Cosine Function
 plot(y,x,'.'); % Plot Sine Wave
-grid on;  %Display the grids on the graphs
-hold on;
-plot(y,z,'*');
+grid on;       % Display the grids on the graphs
+hold on;       % Holds the figure to plot on the same plot the next graph
+plot(y,z,'*'); % Plot of Cosine Function
+legend ('Sine Function', 'Cosine Function');
+title('Sine Function from 0 to 2{\pi}');
+ylabel('Amplitude'); xlabel('Time (in radians)');
+xticks([0 pi/2 pi 3*pi/2 2*pi])
+xticklabels({'0','\pi/2','\pi','3\pi/2','2\pi'});
+xtickangle(45);
 axis ([0 6.2832 -2 2]);
-
-%% Question 13-15
-
-%Parameters
-rng(22)
-xmin=-100; xmax=100;
-ymin=-1.25; ymax=1.25;
-Npts=2^8;
-
-%Variables
-
-xr=linspace(xmin,xmax,Npts);
-ydata=2*(rand(1,Npts)-0.5 *ones (1,Npts));
-ymean=mean(ydata)*ones(size(ydata));
-%figure;
-plot(xr,ydata,'b-.');
-hold on 
-plot(xr,ymean,'c*');
-legend('data','mean');
-grid on 
-
-axis([xmin xmax ymin ymax]);
-%%
-%Question 14
-clear all;
-close all;
-clc;
-%Parameters;
-rng(832);
-xmin=-100; xmax=100;
-ymin=-1.25; ymax=1.25;
-Npts=5*2^8;
-Nss=5; %Sample Size
-sum=0;
-MAdata=ones(5,5);
-Final_Result=strings(5,5);
-
-%Variables
-testvector=rand(25,1);
-Adata=reshape(testvector,[Nss,Nss])';
-%overallmean1=mean(Adata)*ones(size(Adata))
-overallmean=mean(testvector)*ones(size(Adata))
-for r=1:5
-    for c=1:5
-        sum=sum + Adata(r,c);
-        MAdata(r,c)=sum/c
-        if (MAdata(r,c)< overallmean)
-            Final_Result(r,c)= 'Less';
-        elseif (MAdata(r,c) > overallmean)
-            Final_Result(r,c)= 'Greater';
-        else
-            Final_Result(r,c)= 'Equal';
-        end
-    end
-    sum=0;
-end
-
-%%
-% Question 15
-
-tr=linspace(0,2*pi,1000);
-omega=4;
-
-y1=sin(omega*tr);
-y2=cos(2*omega*tr);
-y3=sin(2*omega*tr);
-y4=cos(omega*tr);
-ytotal=[y1;y2;y3;y4];
-
-z=sumfuncs(ytotal);
-
-plot(z)
+hold off;
