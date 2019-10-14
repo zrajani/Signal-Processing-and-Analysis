6c6db793a03af4a0a3e3d5759a4e6e6396995d5c
Zain Anwar Rajani <zainrajani2207@gmail.com>
2019-10-14 16:34:46 -0400

    Update arbitary_terms_calculate.m

--- Basic_Matlab/arbitary_terms_calculate.m
+++ Basic_Matlab/arbitary_terms_calculate.m
@@ -2,15 +2,15 @@
 % The sum of arbitary sinusoidal terms is calculated by creating a function
 % and then calling it from the script or from command window
 
-tr=linspace(0,2*pi,1000);
+tr=linspace(0,2*pi,1000);  % No of points for the arbitary sums
 omega=4;
-
-y1=sin(omega*tr);
+% Arbitary terms
+y1=sin(omega*tr);  
 y2=cos(2*omega*tr);
 y3=sin(2*omega*tr);
 y4=cos(omega*tr);
-ytotal=[y1;y2;y3;y4];
+ytotal=[y1;y2;y3;y4];  % Compute Sum Matrix
 
-z=sumfuncs(ytotal);
+z=sumfuncs(ytotal);  % Function Call to find the total value
 
-plot(z);
+plot(z); % Plot the signal 
