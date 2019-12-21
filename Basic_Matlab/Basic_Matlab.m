d77e31300a4830ebaade4e79a442e0e7c634ee56
zrajani <zainrajani2207@gmail.com>
2019-12-20 22:11:09 -0500

    Minor Changes

--- Basic_Matlab/Basic_Matlab.m
+++ Basic_Matlab/Basic_Matlab.m
@@ -163,3 +163,60 @@ view([-125  30])
 xlabel('Time', 'Rotation',-30)
 ylabel('Real Axis', 'Rotation',10)
 zlabel('Imag Axis')
+
+%% mean of samples and drawing line
+
+%Parameters
+rng(22)
+xmin=-100; xmax=100;
+ymin=-1.25; ymax=1.25;
+Npts=2^8;
+
+%Variables
+
+xr=linspace(xmin,xmax,Npts);
+ydata=2*(rand(1,Npts)-0.5 *ones (1,Npts));
+ymean=mean(ydata)*ones(size(ydata));
+%figure;
+plot(xr,ydata,'b-.');
+hold on 
+plot(xr,ymean,'c*');
+legend('data','mean');
+grid on 
+
+axis([xmin xmax ymin ymax]);
+
+%% Use of if else and for loop
+
+clear all;
+close all;
+clc;
+%Parameters;
+rng(832);
+xmin=-100; xmax=100;
+ymin=-1.25; ymax=1.25;
+Npts=5*2^8;
+Nss=5; %Sample Size
+sum=0;
+MAdata=ones(5,5);
+Final_Result=strings(5,5);
+
+%Variables
+testvector=rand(25,1);
+Adata=reshape(testvector,[Nss,Nss])';
+%overallmean1=mean(Adata)*ones(size(Adata))
+overallmean=mean(testvector)*ones(size(Adata))
+for r=1:5
+    for c=1:5
+        sum=sum + Adata(r,c);
+        MAdata(r,c)=sum/c
+        if (MAdata(r,c)< overallmean)
+            Final_Result(r,c)= 'Less';
+        elseif (MAdata(r,c) > overallmean)
+            Final_Result(r,c)= 'Greater';
+        else
+            Final_Result(r,c)= 'Equal';
+        end
+    end
+    sum=0;
+end
